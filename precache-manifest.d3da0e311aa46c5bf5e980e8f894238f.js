3e4b4bd02ef72eba5dc80fd8359c00ceb05628aa
Marcy <andreeagrijuc@yahoo.com>
2020-09-19 09:12:37 +0300

    Updates

---
+++ precache-manifest.d3da0e311aa46c5bf5e980e8f894238f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851d32efbf7bc62a612c0849d74bd344",
+    "url": "/andreeagrijuc/quotes-and-background/index.html"
+  },
+  {
+    "revision": "821679360863254a6448",
+    "url": "/andreeagrijuc/quotes-and-background/static/css/main.4c316c64.chunk.css"
+  },
+  {
+    "revision": "1d8436aae8d2ccfcb759",
+    "url": "/andreeagrijuc/quotes-and-background/static/js/2.8f582c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andreeagrijuc/quotes-and-background/static/js/2.8f582c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821679360863254a6448",
+    "url": "/andreeagrijuc/quotes-and-background/static/js/main.44fe029c.chunk.js"
+  },
+  {
+    "revision": "04485fcce63e2465fa33",
+    "url": "/andreeagrijuc/quotes-and-background/static/js/runtime-main.0a6b2ddd.js"
+  }
+]);
